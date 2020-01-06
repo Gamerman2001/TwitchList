fd50b8f2e93fd200e73ab4fcd11ff308bb08a08f
Gamerman2001 <Tro12185@gmail.com>
2020-01-06 03:36:20 -0500

    adding mapping function to game streams

--- src/Components/Streams.js
+++ src/Components/Streams.js
@@ -11,59 +11,63 @@ export default function Streams() {
       let baseURL = `https://api.twitch.tv/helix/games?`;
       let queryParams = "";
       gameIDs.map(id => {
-        return queryParams += `id=${id}&`;
+        return (queryParams += `id=${id}&`);
       });
       let finalURL = baseURL + queryParams;
-      let gameNames = await api.get(finalURL)
-      let gameNameArray = gameNames.data.data
-      let finalArray = dataArray.map((stream)=> {
-        stream.gameName = ""
-        gameNameArray.map((name) => {
-            if(stream.game_id === name.id) {
-                return stream.gameName = name.name
-            } else {
-                return stream.gameName 
-            }
-        })
+      let gameNames = await api.get(finalURL);
+      let gameNameArray = gameNames.data.data;
+      let finalArray = dataArray.map(stream => {
+        stream.gameName = "";
+        gameNameArray.map(name => {
+          if (stream.game_id === name.id) {
+            return (stream.gameName = name.name);
+          } else {
+            return stream.gameName;
+          }
+        });
         let newURL = stream.thumbnail_url
-            .replace("width", "300")
-            .replace("height", "300")
-        stream.thumbnail_url = newURL
-        return stream
-      })
-      setChannels(finalArray)
+          .replace("width", "300")
+          .replace("height", "300");
+        stream.thumbnail_url = newURL;
+        return stream;
+      });
+      setChannels(finalArray);
     };
     fetchData();
   }, []);
 
-  return <div>
-      <h1 className='text-center'> Popular Games</h1>
-      <div className='row'>
-        {games.map(game => (
-            
-          <div  key={game.id} className="col-4">
-              <div className='card mt-5'>
-                <img className='card-img-top' src={game.box_art_url} alt={game.name} />
-                <div className='card-body text-center'>
-                    <h5 className='card-title'>{game.name}</h5>
-                    <button className='btn btn-success '>
-                        <Link 
-                        className='link' 
-                        to={{
-                            pathname: 'game/' + game.name,
-                            state: {
-                                gameID: game.id
-                            }
-                        }}
-                        >
-                            {game.name} streams{" "} 
-                        </Link>
-                    </button>
-
-                </div>
+  return (
+    <div>
+      <h1 className="text-center"> Popular Games</h1>
+      <div className="row">
+        {channels.map(game => (
+          <div key={game.id} className="col-4">
+            <div className="card mt-5">
+              <img
+                className="card-img-top"
+                src={game.box_art_url}
+                alt={game.name}
+              />
+              <div className="card-body text-center">
+                <h5 className="card-title">{game.name}</h5>
+                <button className="btn btn-success ">
+                  <Link
+                    className="link"
+                    to={{
+                      pathname: "game/" + game.name,
+                      state: {
+                        gameID: game.id
+                      }
+                    }}
+                  >
+                    {game.name} streams{" "}
+                  </Link>
+                </button>
               </div>
+            </div>
           </div>
         ))}
       </div>
-  </div>;
+    </div>
+  );
 }
